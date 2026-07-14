079b533d269cb8b2e80c53d0705f0de51e1d0d20
srinivasr <sriniv4sreddy@gmail.com>
2026-07-14 16:23:41 +0530

    bump dependencies and nix flake

--- flake.nix
+++ flake.nix
@@ -31,5 +31,16 @@
           program = "${self.packages.${system}.default}/bin/nirimod";
         };
       });
+
+      devShells = forAllSystems (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = with pkgs; [
+            uv
+          ];
+        };
+      });
     };
 }
